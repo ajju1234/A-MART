83dbed750efa8a21e0b995071c988758928a342b
rishabhpandey009 <rishabhpandey009@gmail.com>
2020-09-23 15:01:23 +0530

    Modified some settings

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = environ['gn)l@2bqd%t8)zf(oecidlw@3^tlq=2h63v-p1=%8ju$%j6v(g']
+SECRET_KEY = environ['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
